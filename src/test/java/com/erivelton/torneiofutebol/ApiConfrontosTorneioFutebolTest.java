d79fa75ab355d8162d9d9f015990f9a141f4f7bd
eriveltonluiz <erivelton-paulista@hotmail.com>
2022-09-24 17:16:32 -0300

    Projeto inicial

---
+++ src/test/java/com/erivelton/torneiofutebol/ApiConfrontosTorneioFutebolTest.java
@@ -0,0 +1,21 @@
+package com.erivelton.torneiofutebol;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ApiConfrontosTorneioFutebolTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
